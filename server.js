3f7569b87f52ea22485d62c6a3638166f2b5f0eb
RuoyuWang <klaus1201810802@gmail.com>
2020-07-05 22:26:28 +0100

    add postbuild

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 //Use Middleware
 app.use(express.json({ extended: false }));
-// app.use(cors());
+app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({ msg: 'Hello, this is the contact manager API.' });
@@ -19,15 +19,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // app.use(express.static('client/build'));
 
