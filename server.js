32f47201006f6f25b26f4cc10f0e8c8272ab1438
RuoyuWang <klaus1201810802@gmail.com>
2020-07-05 13:58:38 +0100

    fix dir route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get('/api', (req, res, next) => {
+app.get('/api', (req, res) => {
   res.json({ msg: 'Hello, this is the contact manager API.' });
 });
 
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build'));
   });
 }
 
