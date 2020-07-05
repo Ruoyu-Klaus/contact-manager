f224963fbaaa0852cc195006abda006dfcacec90
RuoyuWang <klaus1201810802@gmail.com>
2020-07-05 13:43:41 +0100

    ready for depolyment

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
 connectDB();
 const app = express();
 
-//User middleware
+//Use Middleware
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res, next) => {
+app.get('/api', (req, res, next) => {
   res.json({ msg: 'Hello, this is the contact manager API.' });
 });
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.port || 4000;
 
